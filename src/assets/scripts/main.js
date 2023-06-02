9adf4f176c465d05b0189b6671684eb44b904be5
nacaru-w <ignaciocr92@gmail.com>
2023-06-02 21:29:01 +0200

    Removing unused js code

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,15 +9,3 @@
 /**
  * Write any other JavaScript below
  */
-// Opens and closes navigation menu
-
-const primaryHeader = document.querySelector('.primary-header')
-const navToggle = document.querySelector(".mobile-nav-toggle");
-const primaryNav = document.querySelector(".primary-navigation");
-const warnMessage = document.querySelector(".alert-info");
-
-navToggle.addEventListener("click", () => {
-    primaryNav.hasAttribute("data-visible") ? navToggle.setAttribute("aria-expanded", false) : navToggle.setAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible");
-    primaryHeader.toggleAttribute("data-overlay")
-})
